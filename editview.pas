b0c6387d99e3238d24e6093f56c1ec7233eca5a1
ikekz <ikekz@mail.ru>
2015-04-24 23:47:55 +1000

    Exchange edits to comboboxes into inner joined fields

--- editview.pas
+++ editview.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, MetaData,
-  sqldb, StdCtrls, DBGrids, SQLRequest;
+  sqldb, DB, StdCtrls, DBGrids, DBCtrls, SQLRequest;
 
 type
 
@@ -14,6 +14,7 @@ type
 
   TEditViewForm = class(TForm)
     ApplyButton: TButton;
+    DataSource: TDataSource;
     DynamicLabel: TLabel;
     DynamicEdit: TEdit;
     SQLQuery: TSQLQuery;
@@ -22,7 +23,7 @@ type
       CurrentTag: integer); static;
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
   private
-    EditArray: array of TEdit;
+    ObjectArray: array of TObject;
     UpdateID: integer;
   public
     { public declarations }
@@ -52,22 +53,25 @@ begin
     for i := 0 to High(Table.FieldArray) do
     begin
       Tag := CurrentTag;
-      if Table.FieldArray[i].Visible then
+      //if Table.FieldArray[i].Visible then
+      //begin
+      DynamicLabel := TLabel.Create(DynamicLabel);
+      with DynamicLabel do
       begin
-        DynamicLabel := TLabel.Create(DynamicLabel);
-        with DynamicLabel do
-        begin
-          Parent := EditViewForm;
-          Left := 10;
-          Top := 35 + 35 * j;
-          Tag := i;
-          Width := 140;
-          Height := 21;
-          Caption := Table.FieldArray[i].Caption;
-        end;
-        SetLength(EditArray, Length(EditArray) + 1);
-        EditArray[High(EditArray)] := TEdit.Create(EditArray[High(EditArray)]);
-        with EditArray[High(EditArray)] do
+        Parent := EditViewForm;
+        Left := 10;
+        Top := 35 + 35 * j;
+        Tag := i;
+        Width := 140;
+        Height := 21;
+        Caption := Table.FieldArray[i].Caption;
+      end;
+      SetLength(ObjectArray, Length(ObjectArray) + 1);
+      if Table.FieldArray[i].TypeField = 1 then
+      begin
+        ObjectArray[High(ObjectArray)] :=
+          TEdit.Create(TEdit(ObjectArray[High(ObjectArray)]));
+        with TEdit(ObjectArray[High(ObjectArray)]) do
         begin
           Parent := EditViewForm;
           Left := 150;
@@ -79,13 +83,46 @@ begin
           Caption := DBGrid.DataSource.DataSet.FieldByName(
             Table.FieldArray[i].Name).AsString;
         end;
-        Inc(j);
+
+      end
+      else
+      begin
+        ObjectArray[High(ObjectArray)] :=
+          TDBComboBox.Create(TDBComboBox(ObjectArray[High(ObjectArray)]));
+        with TDBComboBox(ObjectArray[High(ObjectArray)]) do
+        begin
+          Parent := EditViewForm;
+          Left := 150;
+          Top := 35 + 35 * j;
+          Tag := i;
+          Width := 250;
+          Height := 21;
+          //Caption := Table.FieldArray[i].Caption;
+          Caption := DBGrid.DataSource.DataSet.FieldByName(
+            Table.FieldArray[i].Name).AsString;
+        end;
+        with SQLQuery do
+        begin
+          Close;
+          SQL.Text := 'SELECT * FROM ' + TMyJoinField(
+            Table.FieldArray[i]).JoinTableName;
+          Open;
+
+          while not EOF do
+          begin
+            TDBComboBox(ObjectArray[High(ObjectArray)]).Items.Add(Fields[0].AsString);
+            Next;
+          end;
+
+        end;
       end;
+
+      Inc(j);
+      //end;
     end;
     ShowModal;
     //ModalResult := 1;
   end;
-
 end;
 
 procedure TEditViewForm.ApplyButtonClick(Sender: TObject);
@@ -93,19 +130,32 @@ var
   i: integer;
 begin
   //SQLQuery := CreateUpdate(Table: TMyTable);
+  //with SQLQuery do
+  //begin
+  //  Close;
+  //  SQL.Text := CreateUpdate(TableArray[Tag]);
+  //  if not Prepared then
+  //    Prepare;
+  //  for i := 0 to High(ObjectArray) do
+  //    Params[i].AsString := TEdit(ObjectArray[i]).Text;
+  //  ParamByName('p').AsInteger := UpdateID;
+  //  ShowMessage(SQL.Text);
+  //  ExecSQL;
+  //end;
   with SQLQuery do
   begin
     Close;
-    SQL.Text := CreateUpdate(TableArray[Tag]);
-    if not Prepared then
-      Prepare;
-    for i := 0 to High(EditArray) do
-      Params[i].AsString := EditArray[i].Text;
-    ParamByName('p').AsInteger := UpdateID;
+    SQL.Text := CreateSelect(TableArray[Tag]);
     ShowMessage(SQL.Text);
-    ExecSQL;
-  end;
+    Open;
 
+    while not EOF do
+    begin
+      DBComboBox1.Items.Add(Fields[9].AsString);
+      Next;
+    end;
+
+  end;
 end;
 
 procedure TEditViewForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
